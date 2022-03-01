af3d6122b3a42f820f38950f73d93b24f217245c
meowdy <mike.david.johnston@gmail.com>
2022-02-28 18:52:26 -0700

    add mumbai and matic networks

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,21 +1,79 @@
-require("@nomiclabs/hardhat-waffle");
-
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
+require('@nomiclabs/hardhat-waffle')
+require('@nomiclabs/hardhat-web3')
+require('dotenv').config()
+require('@nomiclabs/hardhat-etherscan')
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
-};
+	defaultNetwork: 'hardhat',
+	networks: {
+		hardhat: {},
+		mumbai: {
+			url: 'https://rpc-mumbai.maticvigil.com',
+			accounts: [''],
+		},
+
+		matic: {
+			url: 'https://polygon-rpc.com/',
+			accounts: [],
+		},
+	},
+	etherscan: {
+		// Your API key for Etherscan
+		// Obtain one at https://etherscan.io/
+		apiKey: '',
+	},
+	solidity: {
+		compilers: [
+			{
+				version: '0.4.24',
+			},
+			{
+				version: '0.6.6',
+				settings: {
+					optimizer: {
+						enabled: true,
+						runs: 0,
+					},
+				},
+			},
+			{
+				version: '0.6.12',
+				settings: {
+					optimizer: {
+						enabled: true,
+					},
+				},
+			},
+			{
+				version: '0.7.6',
+				settings: {
+					optimizer: {
+						enabled: true,
+						runs: 0,
+					},
+				},
+			},
+			{
+				version: '0.8.7',
+				settings: {
+					optimizer: {
+						enabled: true,
+						runs: 0,
+					},
+				},
+			},
+			{
+				version: '0.8.0',
+				settings: {
+					optimizer: {
+						enabled: true,
+						runs: 0,
+					},
+				},
+			},
+		],
+	},
+}
