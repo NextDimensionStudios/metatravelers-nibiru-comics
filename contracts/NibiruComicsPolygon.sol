e7d2c36c4cf3860b56fc7e93e48ff9c96d03eb48
Mike Johnston <mike.david.johnston@gmail.com>
2022-03-06 00:30:00 -0700

    add uninit supply

--- contracts/NibiruComicsPolygon.sol
+++ contracts/NibiruComicsPolygon.sol
@@ -45,7 +45,7 @@ contract NibiruComicsPolygon is ERC721, Ownable {
 
     string public uriPrefix = "";
 
-    uint256 public maxSupply = 500;
+    uint256 public maxSupply;
 
     constructor() ERC721("Metatravelers: Nibiru Comics", "NBRUCOM") {}
 
