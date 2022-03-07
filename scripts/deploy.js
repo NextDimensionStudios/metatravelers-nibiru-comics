6218f90d396ddf8287beb4283354160b53f4155e
Jeremy <peace@trippyogi.com>
2022-03-07 10:46:26 -0800

    Add first test

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,55 +1,55 @@
-const hre = require('hardhat')
+const hre = require('hardhat');
 
 const whitelist = [
-	['0x307919c85E1545C2AA2f6276dA1B60DB04648A71', 1],
-	['0x3e4EfeB5BC5f67D8B61EAee5aC61cac6b6ff31E3', 1],
-	['0x1e19AF366e564055E3973952459B2f39d05F1eda', 2],
-]
+    ['0x307919c85E1545C2AA2f6276dA1B60DB04648A71', 1],
+    ['0x3e4EfeB5BC5f67D8B61EAee5aC61cac6b6ff31E3', 1],
+    ['0x1e19AF366e564055E3973952459B2f39d05F1eda', 2],
+];
 
 async function main() {
-	// deploy the contract
-	const ComicsFactory = await hre.ethers.getContractFactory(
-		'NibiruComicsPolygon'
-	)
-	const comics = await ComicsFactory.deploy()
-	await comics.deployed()
-	console.log('Nibiru Comics deployed to:', comics.address)
+    // deploy the contract
+    const ComicsFactory = await hre.ethers.getContractFactory(
+        'NibiruComicsPolygon'
+    );
+    const comics = await ComicsFactory.deploy();
+    await comics.deployed();
+    console.log('Nibiru Comics deployed to:', comics.address);
 
-	// set the supply
-	try {
-		let maxSupply = '4' // change this value to the max supply you want to set
-		let txn = await comics.setMaxSupply(maxSupply)
-		await txn.wait()
-		console.log('Max supply set to:', maxSupply)
-	} catch (error) {
-		console.error(error)
-	}
+    // set the supply
+    try {
+        let maxSupply = '4'; // change this value to the max supply you want to set
+        let txn = await comics.setMaxSupply(maxSupply);
+        await txn.wait();
+        console.log('Max supply set to:', maxSupply);
+    } catch (error) {
+        console.error(error);
+    }
 
-	// set the tokenURI
-	try {
-		let tokenURI = 'ipfs://QmbmwzNK3adHuMkM1BFdqEdp7e7Le8W7SCUpGyZ7PfSyDD/' // change this value to the tokenURI you want to set
-		let txn = await comics.setUriPrefix(tokenURI)
-		await txn.wait()
-		console.log('tokenURI set to:', tokenURI)
-	} catch (error) {
-		console.error(error)
-	}
+    // set the tokenURI
+    try {
+        let tokenURI = 'ipfs://QmbmwzNK3adHuMkM1BFdqEdp7e7Le8W7SCUpGyZ7PfSyDD/'; // change this value to the tokenURI you want to set
+        let txn = await comics.setUriPrefix(tokenURI);
+        await txn.wait();
+        console.log('tokenURI set to:', tokenURI);
+    } catch (error) {
+        console.error(error);
+    }
 
-	// loop through whitelist and mint tokens
-	for (const [address, amount] of whitelist) {
-		try {
-			let txn = await comics._mintLoop(address, amount)
-			await txn.wait()
-			console.log('Minted NFT(s) for', address)
-		} catch (error) {
-			console.error(error)
-		}
-	}
+    // loop through whitelist and mint tokens
+    for (const [address, amount] of whitelist) {
+        try {
+            let txn = await comics._mintLoop(address, amount);
+            await txn.wait();
+            console.log('Minted NFT(s) for', address);
+        } catch (error) {
+            console.error(error);
+        }
+    }
 }
 
 main()
-	.then(() => process.exit(0))
-	.catch(error => {
-		console.error(error)
-		process.exit(1)
-	})
+    .then(() => process.exit(0))
+    .catch(error => {
+        console.error(error);
+        process.exit(1);
+    });
--- test/comic-test.js
+++ test/comic-test.js
@@ -1,4 +1,33 @@
-const { expectRevert } = require('@openzeppelin/test-helpers')
-const { expect } = require('chai')
-const { ethers } = require('hardhat')
-ethers.utils.Logger.setLogLevel(ethers.utils.Logger.levels.ERROR)
+const { expectRevert } = require('@openzeppelin/test-helpers');
+const { expect } = require('chai');
+const { ethers } = require('hardhat');
+ethers.utils.Logger.setLogLevel(ethers.utils.Logger.levels.ERROR);
+
+describe('Comics', () => {
+    const MAX_SUPPLY = 500;
+    let owner, address1;
+    let Comics, comics;
+
+    before(async () => {
+        [owner, address1] = await ethers.getSigners();
+        Comics = await ethers.getContractFactory('NibiruComicsPolygon');
+    });
+
+    beforeEach(async () => {
+        comics = await Comics.deploy();
+        await comics.deployed();
+    });
+
+    it('should revert if non-owner calls setMaxSupply', async () => {
+        await expectRevert(
+            comics.connect(address1).setMaxSupply(MAX_SUPPLY),
+            'Ownable: caller is not the owner'
+        );
+    });
+
+    it('should update the max supply to the expected value', async () => {
+        await comics.connect(owner).setMaxSupply(MAX_SUPPLY);
+        expect(await comics.maxSupply()).to.eq(MAX_SUPPLY);
+    });
+
+});
