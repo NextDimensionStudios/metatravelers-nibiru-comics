cfb144e84402e76bf92898340f8eaa9866d5b989
Mike Johnston <mike.david.johnston@gmail.com>
2022-03-08 15:03:07 -0700

    post ep1 drop

--- scripts/distribute.js
+++ scripts/distribute.js
@@ -1,21 +1,25 @@
 const { ethers } = require('hardhat');
 const csv = require('csvtojson');
 const BigNumber = require('@ethersproject/bignumber').BigNumber;
+const wait = require('wait');
 
 async function main() {
-    const contractAddress = '';
-    const csvPath = './data/<MintingListName>.csv';
+    const contractAddress = '0xa772A8C5Bb0D9723E3aF07652c0d6AD1C5BD4830';
+    const csvPath = './data/genesiss_stone_snapshots_adjusted_3.csv';
     const users = await csv().fromFile(csvPath);
     const Comics = await ethers.getContractFactory('NibiruComicsPolygon');
     const comics = await Comics.attach(contractAddress);
-    
+
     // loop through whitelist and mint tokens
     for (let i = 0; i < users.length; i++) {
         let user = users[i];
         try {
-            let txn = await comics._mintLoop(user.Address, user.Quantity, { gasLimit: BigNumber.from('800000') });
+            let txn = await comics._mintLoop(user.Address, user.Quantity, {
+                gasLimit: BigNumber.from('1000000'),
+            });
             await txn.wait();
-            console.log(`Minted ${user.Quantity} NFT(s) for ${user.Address}`);
+            console.log(`${i}. Minted ${user.Quantity} NFT(s) for ${user.Address}`);
+            await wait(12000);
         } catch (error) {
             console.error(error);
         }
