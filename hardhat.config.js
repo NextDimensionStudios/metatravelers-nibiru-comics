1431cc0ba13cb6c4417231880b078653e8e07939
Jeremy <peace@trippyogi.com>
2022-03-07 21:44:38 -0800

    Add Mumbai RPC

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,83 +1,83 @@
-require('@nomiclabs/hardhat-waffle')
-require('@nomiclabs/hardhat-web3')
-require('@nomiclabs/hardhat-etherscan')
+require('@nomiclabs/hardhat-waffle');
+require('@nomiclabs/hardhat-web3');
+require('@nomiclabs/hardhat-etherscan');
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-	defaultNetwork: 'hardhat',
-	networks: {
-		hardhat: {},
-		mumbai: {
-			url: '',
-			accounts: [''],
-		},
-		rinkeby: {
-			url: '',
-			accounts: [''],
-			gas: 2100000,
-			gasPrice: 8000000000,
-		},
-		matic: {
-			url: 'https://polygon-rpc.com/',
-			accounts: [''],
-		},
-	},
-	etherscan: {
-		// Your API key for Etherscan
-		// Obtain one at https://etherscan.io/
-		apiKey: '',
-	},
-	solidity: {
-		compilers: [
-			{
-				version: '0.4.24',
-			},
-			{
-				version: '0.6.6',
-				settings: {
-					optimizer: {
-						enabled: true,
-						runs: 0,
-					},
-				},
-			},
-			{
-				version: '0.6.12',
-				settings: {
-					optimizer: {
-						enabled: true,
-					},
-				},
-			},
-			{
-				version: '0.7.6',
-				settings: {
-					optimizer: {
-						enabled: true,
-						runs: 0,
-					},
-				},
-			},
-			{
-				version: '0.8.7',
-				settings: {
-					optimizer: {
-						enabled: true,
-						runs: 0,
-					},
-				},
-			},
-			{
-				version: '0.8.0',
-				settings: {
-					optimizer: {
-						enabled: true,
-						runs: 0,
-					},
-				},
-			},
-		],
-	},
-}
+    defaultNetwork: 'hardhat',
+    networks: {
+        hardhat: {},
+        mumbai: {
+            url: 'https://rpc-mumbai.maticvigil.com',
+            accounts: [''],
+        },
+        rinkeby: {
+            url: '',
+            accounts: [''],
+            gas: 2100000,
+            gasPrice: 8000000000,
+        },
+        matic: {
+            url: 'https://polygon-rpc.com/',
+            accounts: [''],
+        },
+    },
+    etherscan: {
+        // Your API key for Etherscan
+        // Obtain one at https://etherscan.io/
+        apiKey: '',
+    },
+    solidity: {
+        compilers: [
+            {
+                version: '0.4.24',
+            },
+            {
+                version: '0.6.6',
+                settings: {
+                    optimizer: {
+                        enabled: true,
+                        runs: 0,
+                    },
+                },
+            },
+            {
+                version: '0.6.12',
+                settings: {
+                    optimizer: {
+                        enabled: true,
+                    },
+                },
+            },
+            {
+                version: '0.7.6',
+                settings: {
+                    optimizer: {
+                        enabled: true,
+                        runs: 0,
+                    },
+                },
+            },
+            {
+                version: '0.8.7',
+                settings: {
+                    optimizer: {
+                        enabled: true,
+                        runs: 0,
+                    },
+                },
+            },
+            {
+                version: '0.8.0',
+                settings: {
+                    optimizer: {
+                        enabled: true,
+                        runs: 0,
+                    },
+                },
+            },
+        ],
+    },
+};
