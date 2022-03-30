835b992d4ae7a8c2354180bee3a9e247ff5d4f78
meowdy <mike.david.johnston@gmail.com>
2022-03-30 17:39:26 -0600

    clean up distribute file

--- scripts/distribute.js
+++ scripts/distribute.js
@@ -1,12 +1,10 @@
 const fs = require('fs');
-// const wait = require('wait');
 const { ethers } = require('hardhat');
 const csv = require('csvtojson');
-// const BigNumber = require('@ethersproject/bignumber').BigNumber;
 
 async function main() {
     const contractAddress = '0xa772A8C5Bb0D9723E3aF07652c0d6AD1C5BD4830';
-    const csvPath = './data/MetaTravelers_Comics_Minting_List_-_Episode_2_Final.csv';
+    const csvPath = './data/MetaTravelers_Comics_Minting_List_-_Episode_4_Initial.csv';
     const users = await csv().fromFile(csvPath);
     const Comics = await ethers.getContractFactory('NibiruComicsPolygon');
     const comics = await Comics.attach(contractAddress);
@@ -24,7 +22,6 @@ async function main() {
             fs.appendFileSync('distribute_log.txt', `${i + 2}. Minted ${user.Quantity} NFT(s) for ${user.Address}\n`, function (err) {
                 if (err) throw err;
             });
-            // await wait(9000);
         } catch (error) {
             console.error(error);
             fs.appendFileSync('distribute_log.txt', 'missing\n', function (err) {
@@ -33,26 +30,6 @@ async function main() {
         }
     }
 }
-
-
-// const infuraUrl = 'https://polygon-mainnet.infura.io/v3/837cb63ffadc4296869619fcdb832f98';
-// const provider = ethers.getDefaultProvider(infuraUrl, {
-//     infura: {
-//         projectId: '837cb63ffadc4296869619fcdb832f98',
-//         projectSecret: '1f40d17dd0284113a252a372113895e8',
-//     },
-// });
-
-
-// async function waitForTrnx(hash) {
-//     const receipt = await provider.waitForTransaction(hash);
-//     if (receipt.status != 1) {
-//         const errMessage = `Error minting, stopping. Hash: ${hash}`;
-//         console.log(errMessage);
-//         fs.appendFileSync('distribute_log.txt', errMessage);
-//         throw new Error(errMessage);
-//     }
-// }
 
 main()
     .then(() => process.exit(0))
